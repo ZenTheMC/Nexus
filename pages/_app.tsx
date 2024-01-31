22a9e8ea55122bab0fe84121ec25ca8dc1ba09b2
Zen <amaar5ali@gmail.com>
2024-01-30 21:27:12 -0500

    added title, and both icons to app data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <title>Nexus</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
